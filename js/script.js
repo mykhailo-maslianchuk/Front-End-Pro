ad02f8e84c1e4cb046db66515013269b6de09783
Mykhailo Maslianchuk <mykhailo.maslianchuk@gmail.com>
2025-06-08 23:33:02 +0300

    Homework-13

--- js/script.js
+++ js/script.js
@@ -3,6 +3,11 @@
 let userInfoTime = +prompt(`Вкажіть будь ласка кількість годин:`);
     if(isNaN(userInfoTime)) {
         alert('Введіть будь ласка числове значення!');
+        userInfoTime = +prompt(`Вкажіть будь ласка кількість годин:`);
+        if((userInfoTime)) {
+            const userInfoSeconds = (userInfoTime * 60)*60;
+            alert(`У ${userInfoTime} годині(ах) міститься ${userInfoSeconds} секунд.`)
+        }
     }else {
         const userInfoSeconds = (userInfoTime * 60)*60;
         alert(`У ${userInfoTime} годині(ах) міститься ${userInfoSeconds} секунд.`)
