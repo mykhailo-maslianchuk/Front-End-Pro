5f27177b3b22f495d750033ff8b1ed692c53f226
Mykhailo Maslianchuk <mykhailo.maslianchuk@gmail.com>
2025-06-17 22:30:27 +0300

    Homework-17

--- js/script.js
+++ js/script.js
@@ -1,105 +1,67 @@
 'use strict'
 
-console.log(`1. Вивести на сторінку в один рядок через кому числа від 10 до 20.`);
+console.log(`1. Вивести числа від 20 до 30 через пропуск, використовуючи крок 0,5 (20 20,5 21 21,5….).`);
 let resultNamSrc = ``;
-    for (let i = 10; i <=20; i++){
+    for (let i = 20; i <=30; i += 0.5){
         resultNamSrc += i;
-            if(i < 20){
+            if(i < 30){
                 resultNamSrc += `, `;
             }
     }
-console.log(`   Числа від 10 до 20: ${resultNamSrc}`);
+console.log(`   Числа від 20 до 30: ${resultNamSrc}`);
 console.log(`\n`);
-console.log(`2. Вивести квадрати чисел від 10 до 20.`);
-let resultNamSquare = 0;
-    for (let i = 10; i <= 20; i++){
-        console.log(`   Квадрат числа: ${i} = ${resultNamSquare = i * i}`);
+console.log(`2. Один долар коштує 27 гривень. Вивести дані з розрахунком вартості 10, 20, 30... 100 доларів.`);
+const usdUah = 27;
+    for(let i = 10; i <= 100; i += 10){
+        let convUsdUah = i * usdUah;
+        console.log(`   ${i} USD = ${convUsdUah} UAH`);
     }
 console.log(`\n`);
-console.log(`3. Вивести таблицю множення на 7.`);
-for(let i = 1; i <= 10; i++){
-    let multiTableSeven = 7 * i;
-    console.log(`   7 × ${i} = ${multiTableSeven}`)
-}
-console.log(`\n`);
-console.log(`4. Знайти суму всіх цілих чисел від 1 до 15.`);
-let resultSum = 0;
-    for (let i =1; i <= 15; i++){
-        resultSum += i;
-    }
-console.log(`   Сума від 1 до 15: ${resultSum}`);
-console.log(`\n`);
-console.log(`5. Знайти добуток усіх цілих чисел від 15 до 35.`);
-let product = 15n;
-    for (let i = 16n; i <= 35n; i++) {
-        let prevProduct = product;
-        product *= i;
-        console.log(`   ${prevProduct} × ${i} = ${product}`);
-    }
-console.log(`\n`);
-console.log(`6. Знайти середнє арифметичне всіх цілих чисел від 1 до 500.`);
-let namSum = 0;
-let namConst = 0;
-    for (let i = 1; i <= 500; i++) {
-        namSum += i;
-        namConst++;
- }
-let average = namSum / namConst;
-console.log(`   Середнє арифметичне значення: ${average}`);
-console.log(`\n`);
-console.log(`7. Вивести суму лише парних чисел в діапазоні від 30 до 80.`);
-let evenSum = 0;
-    for (let i = 30; i <= 80; i++) {
-        if (i % 2 === 0) {
-            evenSum += i;
+console.log(`3. Дане ціле число. Вивести всі цілі числа від 1 до 100, квадрат яких не перевищує числа N.`);
+const N = 5000;
+    for (let i = 1; i <= 100; i++) {
+        let squareResult = i * i;
+        if  (squareResult <= N ) {
+            console.log(`   ${i}^2 = ${squareResult}`);
         }
     }
-console.log(`   Сума парних чисел: ${evenSum}`);
 console.log(`\n`);
-console.log(`8. Вивести всі числа в діапазоні від 100 до 200 кратні 3.`)
-let multiples = [];
-for (let i = 100; i <= 200; i++) {
-    if (i % 3 === 0) {
-        multiples += (`${i}, `);
-    }
-}
-console.log(`   Числа кратні 3: ${multiples}`);
-console.log(`\n`);
-console.log(`9. Дано натуральне число. Знайти та вивести на сторінку всі його дільники.`)
-let naturalNum = 550;
-let divisors = [];
-    for (let i = 1; i <= naturalNum; i++) {
-        if (naturalNum % i === 0) {
-            divisors += (`${i}, `);
+console.log(`4. Дане ціле число. З'ясувати, чи є воно простим (простим називається число, більше 1, які не мають інших дільників крім 1 і себе)`)
+let simpleNumber = 22
+let isPrime = true;
+    if (simpleNumber <= 1) {
+        isPrime = false;
+    } else {
+        for (let i = 2; i <= Math.sqrt(simpleNumber); i++) {
+            if (simpleNumber % i === 0) {
+                isPrime = false;
+                break;
+            }
         }
     }
-console.log(`   Дільники числа ${naturalNum} = ${divisors}`);
-console.log(`\n`);
-console.log(` 10. Визначити кількість його парних дільників.`)
-let evenDivisorCount = 0;
-let evenDivisorSum = 0;
-let evenDivisors = ``;
-    for (let i = 1; i <= naturalNum; i++) {
-        if (naturalNum % i === 0 && i % 2 === 0) {
-            evenDivisors += (`${i}, `);
-            evenDivisorCount++;
-            evenDivisorSum += i;
-        }
-}
-console.log(`   Парні дільники: ${evenDivisors}`);
-console.log(`   Кількість парних дільників: ${evenDivisorCount}`);
-console.log(`\n`);
-console.log(` 11. Знайти суму його парних дільників.`)
-console.log(`   Сума парних дільників: ${evenDivisorSum}`);
-console.log(`\n`);
-console.log(`12 Надрукувати повну таблицю множення від 1 до 10.`);
-for (let i = 1; i <= 10; i++) {
-    console.log(`\nМноження на ${i}:`);
-    for (let j = 1; j <= 10; j++) {
-        console.log(`   ${i} × ${j} = ${i * j}`);
+    if (isPrime) {
+        console.log(`Число ${simpleNumber} — просте (ділиться тільки на 1 і себе)`);
+    } else {
+        console.log(`Число ${simpleNumber} — не є простим (має інші дільники)`);
     }
-}
-
+console.log(`\n`)
+console.log(`5. Дане деяке число. Визначити, чи можна одержати це число шляхом зведення числа 3 у деякий ступінь. (Наприклад, числа 9, 81 можна отримати, а 13 - не можна).)`);
+let target = 81;
+let isPowerOfThree = false;
+    for (let power = 0; ; power++) {
+        let result = 3 ** power;
+        if (result === target) {
+            console.log(`   Число ${target} = 3^${power} можна отримати`);
+            isPowerOfThree = true;
+            break;
+        }
+        if (result > target) {
+            break;
+        }
+    }
+    if (!isPowerOfThree) {
+        console.log(`   Число ${target} не можна отримати шляхом зведення 3 у деякий ступінь`);
+    }
 
 
 
