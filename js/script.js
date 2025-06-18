c7b358a7d48739dec9b2a9137888ba3a71b76326
Mykhailo Maslianchuk <mykhailo.maslianchuk@gmail.com>
2025-06-18 20:31:24 +0300

    Homework-19

--- js/script.js
+++ js/script.js
@@ -1,25 +1,26 @@
 'use strict'
 
 function greetUser(from, text){
-    alert(`${text}, ${from}!`)
+     alert(`${text}, ${from}!`)
 }
 
 let userName = prompt('Вкажіть будь ласка ваше імʼя: ');
-if (userName === null || userName === '') {
-    alert(`Шкода, що Ви не захотіли вказати своє імʼя.`);
-    userName = `Користувач не вказав імʼя.`;
-}else if(!isNaN(userName)){
-    alert(`Ви ввели некоректне значення!\nВведіть ваше імʼя не в числовому значенні.`);
-    userName = prompt('Ваше імʼя ?');
-    if (!isNaN(userName) || userName === null  || userName === '') {
-        alert(`Шкода, що Ви не захотіли вказати своє імʼя.`);
-        userName = `Користувач не вказав імʼя`;
+    if (userName === null || userName === '') {
+        alert(`Шкода, що Ви не захотіли вказати своє імʼя`);
+        userName = `Користувач не вказав імʼя.`;
+    }else if(!isNaN(userName)){
+        alert(`Ви ввели некоректне значення!\nВведіть ваше імʼя не в числовому значенні.`);
+        userName = prompt('Ваше імʼя ?');
+        if (!isNaN(userName) || userName === null  || userName === '') {
+            alert(`Шкода, що Ви не захотіли вказати своє імʼя`);
+            userName = `Користувач не вказав імʼя`;
+        }
     }
-}
 
 const messageWelcome = `Привіт`
 
 greetUser(userName, messageWelcome);
+
 
 
 
