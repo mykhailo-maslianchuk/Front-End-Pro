ad7ee874b2181b560cd424da97a13107cbcf5f1e
Mykhailo Maslianchuk <mykhailo.maslianchuk@gmail.com>
2025-06-30 09:06:29 +0300

    Homework-26

--- js/script.js
+++ js/script.js
@@ -1,40 +1,146 @@
-'use strict'
+'use strict';
 
-// 1. Ваша функція повинна працювати аналогічно методу array.shift
-const arr= [`Михайло`,`Вероніка`,`Анастасія`,`Марія`,`Василь`,`Назар`];
+const arr = [10, 20, 25, 30, 40, 20, 50];
+let value = 20;
 
-const arrShift = (arr) => {
-    if (arr.length === 0) return undefined;
-
-    const firstElement = arr[0];
-
-    for (let i = 0; i < arr.length - 1; i++) {
-        arr[i] = arr[i + 1];
+// indexOf()
+const indexOf = (arr, value) => {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === value) {
+            return i;
+        }
     }
+    return -1;
+};
 
-    arr.length = arr.length - 1;
-    return firstElement;
-}
-console.log(`Видалений елемент масиву: [ ${arrShift(arr)} ]`);
-console.log(`Оновлений масив: [ ${arr.join(` | `)} ]`);
+const index = indexOf(arr, value);
 
-console.log(`------------------------------------------------------------------`)
+console.log(`Поточний масив: [ ${arr.join(' | ')} ]`);
 
-//2 Ваша наступна функція повинна працювати аналогічно методу array.reverse
-
-const  arrReverse =  (arr) => {
-    let start = 0;
-    let end = arr.length - 1;
-
-    while (start < end) {
-        let temp = arr[start];
-        arr[start] = arr[end];
-        arr[end] = temp;
-
-        start++;
-        end--;
-    }
-    return (arr);
+if (index !== -1) {
+    console.log(`Елемент: ${value}`);
+    console.log(`Індекс елемента в масиві: ${index}`);
+} else {
+    console.log(`Елемент "${value}" не знайдено в масиві.`);
 }
 
-console.log(`Перевернутий масив: [ ${arrReverse(arr).join(` | `)} ]`);
+console.log(`-----------------------------------------------`);
+// lastIndexOf()
+
+const lastIndexOf = (arr, value) => {
+    for (let i = arr.length - 1; i >= 0; i--) {
+        if (arr[i] === value) {
+            return i;
+        }
+    }
+    return -1;
+};
+
+const indexlastIndexOf = lastIndexOf(arr, value);
+
+console.log(`Поточний масив: [ ${arr.join(' | ')} ]`);
+
+if (indexlastIndexOf !== -1) {
+    console.log(`Елемент: ${value}`);
+    console.log(`Індекс елемента в масиві: ${indexlastIndexOf}`);
+} else {
+    console.log(`Елемент "${value}" не знайдено в масиві.`);
+}
+
+console.log(`-----------------------------------------------`);
+// find
+
+const find = (arr, value) => {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > value) {
+            return arr[i];
+        }
+    }
+    return undefined;
+};
+
+const resultFind = find(arr, value);
+
+console.log(`Поточний масив: [ ${arr.join(' | ')} ]`);
+console.log(`Перший елемент в масиві більший за ${value}: ${resultFind}`);
+
+console.log(`-----------------------------------------------`);
+
+// findIndex
+
+const findIndex = (arr, value) => {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > value) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+const indexfindIndex = findIndex(arr, value);
+
+console.log(`Масив: [ ${arr.join(' | ')} ]`);
+
+if (index !== -1) {
+    console.log(`Елемент більший за ${value} знайдено на позиції ${indexfindIndex} — це ${arr[indexfindIndex]}`);
+} else {
+    console.log(`Елемент більший за ${value} не знайдено.`);
+}
+
+console.log(`-----------------------------------------------`);
+
+// includes
+
+const includes = (arr, value) => {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === value) {
+            return true;
+        }
+    }
+    return false;
+}
+const isIncluded = includes(arr, value);
+
+console.log(`Поточний масив: [ ${arr.join(' | ')} ]`);
+
+console.log(`Перевіряємо, чи містить масив значення: ${value}`);
+
+if (isIncluded) {
+    console.log(`Так! Масив містить значення "${value}"`);
+} else {
+    console.log(`Ні, масив не містить значення "${value}"`);
+}
+
+//every
+console.log(`-----------------------------------------------`);
+
+const every = (arr, value)  => {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] <= value) {
+            return false;
+        }
+    }
+    return true;
+}
+value = 9
+const resultEvery = every(arr, value);
+
+console.log(`Поточний масив: [ ${arr.join(' | ')} ]`);
+console.log(`Чи всі елементи більші за ${value} ? > ${resultEvery ? 'Так' : 'Ні'}`);
+
+//some
+
+console.log(`-----------------------------------------------`);
+
+function some(arr, value) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > value) {
+            return true;
+        }
+    }
+    return false;
+}
+value = 49
+const resultSome =  some(arr, value)
+console.log(`Поточний масив: [ ${arr.join(' | ')} ]`);
+console.log(`Чи є хоча б один елемент більший за ${value} ? > ${resultSome ? 'Так' : 'Ні'}`);
