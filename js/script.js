d72bfd3379ddf419b169f7c3bd5c5743720dc544
Mykhailo Maslianchuk <mykhailo.maslianchuk@gmail.com>
2025-07-01 20:41:03 +0300

    Homework-27

--- js/script.js
+++ js/script.js
@@ -1,146 +1,74 @@
 'use strict';
 
-const arr = [10, 20, 25, 30, 40, 20, 50];
-let value = 20;
+const arr = [16,-37,54,-4,72,-56,47,4, -16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47]
 
-// indexOf()
-const indexOf = (arr, value) => {
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] === value) {
-            return i;
-        }
+const arrPositives = arr.filter(num => num > 0);
+const arrNegatives = arr.filter(num => num < 0);
+
+const sum = (arr) =>{
+    let sum = 0;
+    for(let i = 0; i < arr.length; i++) {
+        sum += arr[i];
     }
-    return -1;
-};
-
-const index = indexOf(arr, value);
-
-console.log(`Поточний масив: [ ${arr.join(' | ')} ]`);
-
-if (index !== -1) {
-    console.log(`Елемент: ${value}`);
-    console.log(`Індекс елемента в масиві: ${index}`);
-} else {
-    console.log(`Елемент "${value}" не знайдено в масиві.`);
+    return sum;
 }
+const sumPositives = sum(arrPositives);
 
-console.log(`-----------------------------------------------`);
-// lastIndexOf()
+const arrMaxValue = Math.max(...arr);
+const arrMaxIdex = arr.indexOf(arrMaxValue);
 
-const lastIndexOf = (arr, value) => {
-    for (let i = arr.length - 1; i >= 0; i--) {
-        if (arr[i] === value) {
-            return i;
-        }
-    }
-    return -1;
-};
+const arrMinValue = Math.min(...arr);
+const arrMinIdex = arr.indexOf(arrMinValue);
 
-const indexlastIndexOf = lastIndexOf(arr, value);
+const arrEvenPositives = arr.filter(num => num > 0 && num % 2 === 0);
+const sumArrEvenPositives = sum(arrEvenPositives);
+const arrOddPositives = arr.filter(num => num > 0 && num % 2 !== 0);
+const sumArrOddPositives = sum(arrOddPositives);
 
-console.log(`Поточний масив: [ ${arr.join(' | ')} ]`);
+const arrProduct = arrPositives.reduce((acc, curr) => acc * curr, 1)
 
-if (indexlastIndexOf !== -1) {
-    console.log(`Елемент: ${value}`);
-    console.log(`Індекс елемента в масиві: ${indexlastIndexOf}`);
-} else {
-    console.log(`Елемент "${value}" не знайдено в масиві.`);
-}
+const  arrOnlyMax = arr.map(num => num === arrMaxValue ? num : 0)
 
-console.log(`-----------------------------------------------`);
-// find
+console.log(`                          АНАЛІЗ МАСИВУ`                          )
+console.log('-----------------------------------------------------------------');
+console.log(`Поточний масив:\n[ ${arr.join(` | `)} ]`)
+console.log('-----------------------------------------------------------------');
+/*
+1. Дан масив [16,-37,54,-4,72,-56,47,4, -16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47]
+Знайти суму та кількість позитивних елементів.
+*/
+console.log(`Позитивні елементи:\n[ ${arrPositives.join(` | `)} ]`)
+console.log(`Кількість позитивних елементів: ${arrPositives.length}`)
+console.log(`Сума позитивних елементів: ${sumPositives}`)
+console.log('-----------------------------------------------------------------');
+//2. Знайти мінімальний елемент масиву та його порядковий номер.
+console.log(`Мінімальний елемент: ${arrMinValue}\nПорядковий номер елементу: ${arrMinIdex}`)
+console.log('-----------------------------------------------------------------');
+//3. Знайти максимальний елемент масиву та його порядковий номер.
+console.log(`Максимальний елемент: ${arrMaxValue}\nПорядковий номер елементу: ${arrMaxIdex}`)
+console.log('-----------------------------------------------------------------');
+//4. Визначити кількість негативних елементів.
+console.log(`Кількість негативних елементів: ${arrNegatives.length}`)
+console.log('-----------------------------------------------------------------');
+// 5. Знайти кількість непарних позитивних елементів.
+// 8. Знайти суму непарних позитивних елементів.
+console.log(`Непарні позитивні елементи: [ ${arrOddPositives.join(` | `)} ]`)
+console.log(`Кількість непарних позитивних елементів: ${arrOddPositives.length}`)
+console.log(`Сума позитивних непарних елементів: ${sumArrOddPositives}`)
+console.log('-----------------------------------------------------------------');
+// 6. Знайти кількість парних позитивних елементів.
+// 7. Знайти суму парних позитивних елементів.
+console.log(`Парні позитивні елементи: [ ${arrEvenPositives.join(` | `)} ]`)
+console.log(`Кількість парних позитивних елементів: ${arrEvenPositives.length}`)
+console.log(`Сума позитивних парних елементів: ${sumArrEvenPositives}`)
+console.log('-----------------------------------------------------------------');
+// 9. Знайти добуток позитивних елементів.
+console.log(`Позитивні елементи:\n[ ${arrPositives.join(` | `)} ]`)
+console.log(`Добуток позитивних елементів: ${arrProduct}`)
+console.log('-----------------------------------------------------------------');
+// 10. Знайти найбільший серед елементів масиву, остальні обнулити.
+console.log(`Масив, де залишено тільки найбільше число:\n[ ${arrOnlyMax.join(` | `)} ]`)
 
-const find = (arr, value) => {
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] > value) {
-            return arr[i];
-        }
-    }
-    return undefined;
-};
 
-const resultFind = find(arr, value);
 
-console.log(`Поточний масив: [ ${arr.join(' | ')} ]`);
-console.log(`Перший елемент в масиві більший за ${value}: ${resultFind}`);
 
-console.log(`-----------------------------------------------`);
-
-// findIndex
-
-const findIndex = (arr, value) => {
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] > value) {
-            return i;
-        }
-    }
-    return -1;
-}
-
-const indexfindIndex = findIndex(arr, value);
-
-console.log(`Масив: [ ${arr.join(' | ')} ]`);
-
-if (indexfindIndex !== -1) {
-    console.log(`Елемент більший за ${value} знайдено на позиції ${indexfindIndex} — це ${arr[indexfindIndex]}`);
-} else {
-    console.log(`Елемент більший за ${value} не знайдено.`);
-}
-
-console.log(`-----------------------------------------------`);
-
-// includes
-
-const includes = (arr, value) => {
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] === value) {
-            return true;
-        }
-    }
-    return false;
-}
-const isIncluded = includes(arr, value);
-
-console.log(`Поточний масив: [ ${arr.join(' | ')} ]`);
-
-console.log(`Перевіряємо, чи містить масив значення: ${value}`);
-
-if (isIncluded) {
-    console.log(`Так! Масив містить значення "${value}"`);
-} else {
-    console.log(`Ні, масив не містить значення "${value}"`);
-}
-
-//every
-console.log(`-----------------------------------------------`);
-
-const every = (arr, value)  => {
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] <= value) {
-            return false;
-        }
-    }
-    return true;
-}
-value = 9
-const resultEvery = every(arr, value);
-
-console.log(`Поточний масив: [ ${arr.join(' | ')} ]`);
-console.log(`Чи всі елементи більші за ${value} ? > ${resultEvery ? 'Так' : 'Ні'}`);
-
-//some
-
-console.log(`-----------------------------------------------`);
-
-function some(arr, value) {
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] > value) {
-            return true;
-        }
-    }
-    return false;
-}
-value = 49
-const resultSome =  some(arr, value)
-console.log(`Поточний масив: [ ${arr.join(' | ')} ]`);
-console.log(`Чи є хоча б один елемент більший за ${value} ? > ${resultSome ? 'Так' : 'Ні'}`);
