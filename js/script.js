4989dd3be1bcff8183b5dc6093e4bb91f04ba1fe
Mykhailo Maslianchuk <mykhailo.maslianchuk@gmail.com>
2025-07-15 01:07:53 +0300

    Homework-34

--- js/script.js
+++ js/script.js
@@ -1,19 +1,3 @@
 'use strict';
 
-const checkbox = document.getElementById('checkbox');
-const body = document.body;
 
-if (localStorage.getItem('theme') === 'dark') {
-    body.classList.add('dark-theme');
-    checkbox.checked = true;
-}
-
-checkbox.addEventListener('change', function () {
-    body.classList.toggle('dark-theme');
-
-    if (body.classList.contains('dark-theme')) {
-        localStorage.setItem('theme', 'dark');
-    } else {
-        localStorage.setItem('theme', 'light');
-    }
-});
