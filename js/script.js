3c2375ad9eb3e6d1f889f899e3e3ead357f9529f
Mykhailo Maslianchuk <mykhailo.maslianchuk@gmail.com>
2025-07-15 14:58:51 +0300

    Homework-34

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
 'use strict';
 
+const filter = document.getElementById('filter');
 
+const cards = document.querySelectorAll('.card');
+
+filter.addEventListener('change', function () {
+
+    const selectedCategory = this.value;
+
+    cards.forEach(card => {
+
+        const category = card.dataset.category;
+
+        if (selectedCategory === 'all' || category === selectedCategory) {
+            card.style.display = 'block';
+        } else {
+            card.style.display = 'none';
+        }
+    });
+});
