adb2e06a88bd7fadd10905ee659835746a2daf8f
Mykhailo Maslianchuk <mykhailo.maslianchuk@gmail.com>
2025-08-05 17:27:14 +0300

    Homework-38

--- js/script.js
+++ js/script.js
@@ -1,48 +1,90 @@
 'use strict';
 
-function findValuesByKey(obj, targetKey) {
-    const result = [];
+function Student(firstName, lastName, birthYear){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.birthYear = birthYear;
+    this.marks = [];
+    this.attendance = new Array(25).fill(null);
 
-    function search(current) {
-        if (Array.isArray(current)) {
-            for (const item of current) {
-                search(item);
-            }
-        } else if (typeof current === 'object' && current !== null) {
-            for (const key in current) {
-                if (key === targetKey) {
-                    result.push(current[key]);
-                }
-                search(current[key]);
+    this.present = function() {
+        if (!this.attendance.includes(null)) {
+            console.warn("Всі 25 занять уже відмічені!");
+            return;
+        }
+
+        for (let i = 0; i < this.attendance.length; i++) {
+            if (this.attendance[i] === null) {
+                this.attendance[i] = true;
+                break;
             }
         }
+    };
+
+    this.absent = function() {
+        if (!this.attendance.includes(null)) {
+            console.warn("Всі 25 занять уже відмічені!");
+            return;
+        }
+
+        for (let i = 0; i < this.attendance.length; i++) {
+            if (this.attendance[i] === null) {
+                this.attendance[i] = false;
+                break;
+            }
+        }
+    };
+
+    this.getAge =  function() {
+        return new Date().getFullYear() - this.birthYear;
     }
-    search(obj);
-    return result;
+
+    this.getAverageMark =  function() {
+        if(this.marks.length === 0) return 0;
+        let sum = this.marks.reduce((total, val) => total + val, 0);
+        return sum / this.marks.length;
+    }
+
+    this.summary = function (){
+        let avgMark = this.getAverageMark();
+
+        let total = this.attendance.filter(val => val !== null).length;
+        let visited = this.attendance.filter(val => val === true).length;
+        let avgVisit = total === 0 ? 0 : visited / total;
+
+        if (avgMark > 90 && avgVisit > 0.9) {
+            return "Молодець!";
+        } else if (avgMark > 90 || avgVisit > 0.9) {
+            return "Добре, але можна краще";
+        } else {
+            return "Редиска!";
+        }
+    }
+    this.info = function (){
+        let totalLessons = this.attendance.length;
+        let visited = this.attendance.filter(val => val === true).length;
+        let visitRate = totalLessons === 0 ? 0 : (visited / totalLessons * 100).toFixed(2);
+
+        return `Імʼя: ${this.firstName} ${this.lastName}\nВік: ${this.getAge()}\nСередній бал: ${this.getAverageMark().toFixed(2)}\nВідвідування: ${visited}/${totalLessons} (${visitRate}%)`
+    }
 }
 
-const data = {
-    id: 1,
-    name: "root",
-    meta: {
-        id: 2,
-        parent: {
-            id: 3,
-            name: "leaf",
-        },
-    },
-    array: [
-        { id: 4 },
-        { name: "node", children: [{ id: 5 }] },
-    ],
-};
+let student_1 = new Student('John', 'Doe', 2005);
+student_1.present()
+student_1.present()
+student_1.present()
+student_1.present()
+student_1.absent()
+student_1.marks.push(20, 40, 60, 100)
+console.log(student_1.info())
 
-console.log(findValuesByKey(data, "id"));
-
-
-
-
-
-
-
-
+let student_2 = new Student('Mykhailo', 'Maslianchuk', 2004);
+student_2 .present()
+student_2.present()
+student_2.present()
+student_2.present()
+student_2.absent()
+student_2.present()
+student_2.present()
+student_2.marks.push(20, 40, 60, 80)
+console.log(student_2.info())
